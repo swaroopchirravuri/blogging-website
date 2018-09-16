f28150fad7f6e272c2966e3812652451fe0fce4c
Anup Mishra <anup.m@pepperfry.com>
2018-09-16 22:52:05 +0530

    Merging PartB

---
+++ stub/js/bloglist.js
@@ -0,0 +1,11 @@
+
+
+function showModal(){
+        document.getElementById('postModal').style.display = "block";
+}
+
+
+// When the user clicks on the button, open the modal
+document.getElementById('noModalBtn').onclick = function() {
+    document.getElementById('postModal').style.display = "none";
+}
